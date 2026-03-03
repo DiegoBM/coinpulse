15c7a626e42808054ab08d82326038b4bb5d107d
DiegoBM <1613216+DiegoBM@users.noreply.github.com>
2026-03-03 19:05:10 +0100

    implemented homepage overview and trending

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Suspense } from "react";
+import CoinOverview from "@/components/home/CoinOverview";
+import TrendingCoins from "@/components/home/TrendingCoins";
+import CoinOverviewFallback from "@/components/home/CoinOverviewFallback";
+import { TrendingCoinsFallback } from "@/components/home/TrendingCoinsFallback";
 
-const Page = () => {
-  return <div></div>;
-};
-
-export default Page;
+export default function Page() {
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  );
+}
